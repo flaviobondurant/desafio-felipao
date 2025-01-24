c3f05c4a1a671a24e043c549ba63dd242d104524
flaviobondurant <flaviozigo@gmail.com>
2025-01-24 16:03:36 -0300

    desafio completo

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log ("olaaa")
+let NomeHerói = "Pulga Voadora"
+let NivelHerói = 5000
+
+ 
+ 
+ if(NivelHerói <= 1000){
+    console.log(" O Herói de nome " + NomeHerói, "está no nível Ferro")}
+
+ else if (NivelHerói <=2000){
+    console.log(" O Herói de nome " + NomeHerói, "está no nível Bronze")
+ } else if (NivelHerói <=5000){
+    console.log(" O Herói de nome " + NomeHerói, "está no nível Prata")
+ } else if (NivelHerói <=7000){console.log(" O Herói de nome " + NomeHerói, "está no nível Ouro")}
+ else if (NivelHerói <=8000){console.log(" O Herói de nome " + NomeHerói, "está no nível Platina")}
+ else if (NivelHerói <=9000){console.log(" O Herói de nome " + NomeHerói, "está no nível Ferro Ascendente")}
+ else if (NivelHerói <=10000){console.log(" O Herói de nome " + NomeHerói, "está no nível Imortal")}
+  
+ else {console.log(" O Herói de nome " + NomeHerói, "está no nível Radiante")}
