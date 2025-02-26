c4033d724f592f5fcb3737f4c04209da3c52867c
flaviobondurant <flaviozigo@gmail.com>
2025-02-25 22:42:21 -0300

    Terceiro desafio entregue

--- desafio3.js
+++ desafio3.js
@@ -1,36 +1,50 @@
-// class declara uma classe.
-// constructor deve ser sempre a função principal.
-// this substitui o comando let quando dentro de uma classe. ( variavel)
-
-
 
 class heroi{
     constructor(nome, idade, tipo){
             this.nome = nome
             this.idade = idade
             this.tipo = tipo
+        
+        
         }
-
-    ataque(){
-            if (this.tipo = "Mago")
-                ataque = "Magia";
-        }
-
 
         saida(){
-            console.log(`o ${this.tipo} atacou usando ${this.ataque}`)
+            let golpe;
+
+                if  (this.tipo === "Mago"){
+                golpe = "Magia";
+                }
+                else if (this.tipo === "Guerreiro"){
+                golpe = "Espada";
+                 }
+                 else if (this.tipo === "Monge"){
+                golpe = "Artes Marciais";
+                 }
+                 else if (this.tipo === "Ninja"){
+                golpe = "Shoriuken"
+                 }
+
+
+            console.log(`o ${this.tipo} atacou usando ${golpe}`);
         }
 
         
 }
 
 
+
+
+
 let magoo = new heroi ("Magoo", 1200, "Mago")
+let guerreito = new heroi ("Guerreiru", 3000, "Guerreiro")
+
 
 
 magoo.saida()
+guerreito.saida()
 
-// pendente criar estrutuda de f-definição para o ataque de acordo com o tipo do heroi 
+
+
 //se mago -> no ataque exibir (usou magia)
 //se guerreiro -> no ataque exibir (usou espada)
 //se monge -> no ataque exibir (usou artes marciais)
